3ea92a2dfa7c9902ca5eeb181c56faec7c867e24
Jonathan Wehrle <jonathanw@gmx.de>
2012-12-17 02:13:24 +0100

    Set name in config

--- conf.lua
+++ conf.lua
@@ -1,12 +1,12 @@
 function love.conf(t)
 	t.screen.width = 800
 	t.screen.height = 600
-	t.screen.fullscreen = false
+	t.screen.fullscreen = no
 
-	t.title = "unnamed"
+	t.title = "Lovely Santa"
 	t.author = "Jonathan Wehrle"
 
-	t.identity = "unnamed"
+	t.identity = "atnaSylevoL"
 
 	t.version = "0.8.0"
 
